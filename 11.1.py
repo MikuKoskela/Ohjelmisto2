f133c8346890468f384b7fbbd3a795ff4d17202a
Miku Koskela <Miku.Koskela@metropolia.fi>
2022-11-07 16:37:39 +0200

    commit

--- 11.1.py
+++ 11.1.py
@@ -4,27 +4,21 @@
 # Tee aliluokkiin metodi tulosta_tiedot, joka tudostaa kyseisen julkaisun kaikki tiedot.
 # Luo pääohjelmassa julkaisut Aku Ankka (päätoimittaja Aki Hyyppä) ja Hytti n:o 6 (kirjailija Rosa Liksom, 200 sivua).
 # Tulosta molempien julkaisujen kaikki tiedot toteuttamiesi metodien avulla.
-
 class Julkaisu:
     def __init__(self,nimi):
         self.nimi = nimi
-
 class Kirja(Julkaisu):
     def __init__(self,nimi,kirjoittaja,sivumaara):
         super().__init__(nimi)
         self.kirjoittaja = kirjoittaja
         self.sivumaara = sivumaara
 
-
     def tulosta_tiedot(self):
         print(f"Kirjan nimi: {self.nimi}. -- Kirjoittaja: {self.kirjoittaja} -- Sivumäärä: {self.sivumaara}")
-
 class Lehti(Julkaisu):
     def __init__(self,nimi,paatoimittaja):
         super().__init__(nimi)
         self.paatoimittaja = paatoimittaja
-
-
     def tulosta_tiedot(self):
         print(f"Julkaisun nimi: {self.nimi} -- Päätoimittaja: {self.paatoimittaja}")
 
